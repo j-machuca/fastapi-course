c220d79efda1079b2f8e95f5b163b0ea44aecfa2
Jorge Machuca <jmachuca@bond.com.py>
2022-10-31 22:07:20 -0300

    posts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -8,6 +9,11 @@ async def root():
     return {"message": "Hello World!"}
 
 
-@app.get('/posts')
+@app.get("/posts")
 async def get_post():
     return {"data": "This is your data"}
+
+
+@app.post("/posts")
+async def create_posts(payload: dict = Body(...)):
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
